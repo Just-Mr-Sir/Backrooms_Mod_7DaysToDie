5f864547cf130e49f9a6ef1a5894665753f568e4
Just-Mr-Sir <callan109@charter.net>
2024-12-13 14:26:26 -0800

    Add files via upload

---
+++ Source/BackroomsMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackroomsGenV1_1NET4_5
+{
+    public class BackroomsMain : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            IConsoleCommand commandGenBR = new ConsoleCmdGenBR();
+            IConsoleCommand commandDeleteRegions = new ConsoleCmdDeleteRegions();
+            IConsoleCommand commandNoClipBR = new ConsoleCmdNoclipBR();
+            SortedList<string, IConsoleCommand> sl = new SortedList<string, IConsoleCommand>
+            {
+                { "GenerateBackrooms", commandGenBR },
+                { "Delete Regions", commandDeleteRegions },
+                { "Noclip BR", commandNoClipBR }
+            };
+
+            SdtdConsole.Instance.RegisterCommand(sl, "ConsoleCmdGenBR", commandGenBR);
+            SdtdConsole.Instance.RegisterCommand(sl, "ConsoleCmdDeleteRegions", commandDeleteRegions);
+            SdtdConsole.Instance.RegisterCommand(sl, "ConsoleCmdNoclipBR", commandNoClipBR);
+
+            MapGen.UpdateWeightedList();
+        }
+    }
+}
---
+++ Source/ConsoleCmdNoclipBR.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BackroomsGenV1_1NET4_5
+{
+    internal class ConsoleCmdNoclipBR: ConsoleCmdAbstract
+    {
+        public ConsoleCmdNoclipBR() : base()
+        {
+        }
+
+        public override string[] GetCommands()
+        {
+            //command names in this package
+            string[] commands = {
+            "noclipBR"
+        };
+
+            return commands;
+        }
+
+        public override string GetDescription()
+        {
+            return "Deletes all the region files for a specific game";
+        }
+
+        public override string GetHelp()
+        {
+            return "Make sure to use valid commands and arguments: noclipIntoBR";
+        }
+
+        public override void Execute(List<string> _params, CommandSenderInfo _senderInfo)
+        {
+            EntityPlayerLocal epl = GameManager.Instance.myEntityPlayerLocal;
+            Vector3 pos = epl.GetPosition();
+
+            if (pos.y <= 13)
+                SdtdConsole.Instance.executeCommand("teleport " + Math.Round(pos.x) + " " + Math.Round(pos.z), _senderInfo);
+            else
+            {
+                Vector3i testPos = new Vector3i(pos.x, AllRoomData.BASE_OFFSET_Y + 1, pos.z);
+                int breakCounter = 2000;
+                while(!GameManager.Instance.World.IsEmptyPosition(testPos) && breakCounter > 0)
+                {
+                    testPos.x += 1;
+                    testPos.z += 1;
+                    breakCounter--;
+                }
+
+                SdtdConsole.Instance.executeCommand("teleport " + testPos.x + " " + testPos.y + " " + testPos.z, _senderInfo);
+            }
+        }
+
+        public override string[] getCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
